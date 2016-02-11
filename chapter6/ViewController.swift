792ad2039611715b36bb83ee67793c96af6f6db8
Masayuki Morisaki <masayuki144@gmail.com>
2016-02-11 23:33:37 +0900

    メインビュー表示時にランダムにRGBを表示する

--- chapter6/ViewController.swift
+++ chapter6/ViewController.swift
@@ -7,9 +7,26 @@
 //
 
 import UIKit
+import GameKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorLabel: UILabel!
+
+    // ランダム値のRGB
+    let randomSource = GKARC4RandomSource()
+    var colorR = 0
+    var colorG = 0
+    var colorB = 0
+    
+    override func viewWillAppear(animated: Bool) {
+        colorR = randomSource.nextIntWithUpperBound(256)
+        colorG = randomSource.nextIntWithUpperBound(256)
+        colorB = randomSource.nextIntWithUpperBound(256)
+        
+        colorLabel.text = "R:\(colorR) G:\(colorG) B:\(colorB)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
