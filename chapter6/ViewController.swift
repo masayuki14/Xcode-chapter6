93b00810a032a325e2545d74279bb7fedc047071
Masayuki Morisaki <masayuki144@gmail.com>
2016-02-11 22:24:29 +0900

    Initial Commit

---
+++ chapter6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  chapter6
+//
+//  Created by 森崎 雅之 on 2016/02/11.
+//  Copyright © 2016年 森崎 雅之. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
