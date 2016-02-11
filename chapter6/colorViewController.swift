c16d28129eb721c95991eb4a97ada7c7b46b6d96
Masayuki Morisaki <masayuki144@gmail.com>
2016-02-11 22:45:42 +0900

    「色を見る」ボタンタップで画面遷移する

---
+++ chapter6/colorViewController.swift
@@ -0,0 +1,35 @@
+//
+//  colorViewController.swift
+//  chapter6
+//
+//  Created by 森崎 雅之 on 2016/02/11.
+//  Copyright © 2016年 森崎 雅之. All rights reserved.
+//
+
+import UIKit
+
+class colorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
